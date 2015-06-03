ed58c5c0d49b6c313f04b67d662d550400230e45
fffw <fffw@getlantern.org>
2015-06-03 12:07:27 +0000

    configure dkim

--- make.sh
+++ make.sh
@@ -1,3 +1,5 @@
 cat docker/boto.cfg.tmpl | envsubst > docker/boto.cfg
+if [ ! -z docker/dkim.key ]; then
+    cp ../too-many-secrets/mail-responder/dkim.key docker/dkim.key
+fi
 docker build -t mail-responder docker/
-rm docker/boto.cfg
